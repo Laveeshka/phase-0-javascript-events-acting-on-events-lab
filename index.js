6e7804cd01396fc26dabb5dc70f303a4f833bc4e
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-07 18:02:59 +1000

    commit tree 96e85b153bcaf6fdcf1a4495394118477873c7c8

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById("dodger");
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+        dodger.style.left = `${left-1}px`;
+        }
+}
+
+function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const boxWidthLessDodger = 400 - 40;
+    if (left <  boxWidthLessDodger) {
+        dodger.style.left = `${left+1}px`;
+    }
+
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
